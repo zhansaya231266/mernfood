4fc57114547e0b514d711fc4ba0a92afae2f5299
zhansaya231266 <154576616+zhansaya231266@users.noreply.github.com>
2025-02-20 22:23:37 +0500

    Add files via upload

---
+++ middlewares/logger.js
@@ -0,0 +1,15 @@
+import winston from "winston";
+
+const logger = winston.createLogger({
+    level: "info",
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    ),
+    transports: [
+        new winston.transports.File({ filename: "logs/error.log", level: "error" }),
+        new winston.transports.File({ filename: "logs/combined.log" })
+    ]
+});
+
+export default logger;
